e8ed662f5729587e925ee19768db8c3620830859
matthewcotton <matthewneilcotton@gmail.com>
2021-02-04 09:39:00 +0000

    split out authController

---
+++ src/Controllers/authController.js
@@ -0,0 +1,33 @@
+const createErr = require("http-errors");
+// const { ObjectId } = require("mongodb");
+const { v4: uuidv4 } = require("uuid");
+const { User } = require("../../models/users");
+
+// Authentication
+exports.auth = async function (req, res, next) {
+  const user = await User.findOne({
+    username: req.body.username.toLowerCase(),
+  });
+  if (!user) {
+    return next(createErr(401, "Incorrect username"));
+  }
+  // Password check
+  // Make this a hash
+  if (req.body.password !== user.password) {
+    return next(createErr(403, "Incorrect password"));
+  }
+  user.token = uuidv4();
+  await user.save();
+  res.send({ token: user.token });
+};
+
+// Token Check
+exports.tokenCheck = async function (req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const user = await User.findOne({ token: authHeader });
+  if (user) {
+    return;
+  } else {
+    next(createErr(401, "Authentication failed"));
+  }
+};
