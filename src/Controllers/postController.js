52f8d7213338bfbaefc1d1d569801797a24b5ddc
matthewcotton <matthewneilcotton@gmail.com>
2021-03-06 14:00:15 +0000

    add limit and skip agrs to all posts endpoint and return most recent first

--- src/Controllers/postController.js
+++ src/Controllers/postController.js
@@ -5,7 +5,9 @@ const authController = require("./authController");
 
 // Get all posts (unprotected endpoint)
 exports.index = async function (req, res) {
-  res.send(await Post.find());
+  const skip = req.body.skip ? Number(req.body.skip) : 0;
+  const limit = req.body.limit ? Number(req.body.limit) : 10;
+  res.send(await Post.find({}).sort({ timestamp: -1 }).skip(skip).limit(limit));
 };
 
 // Get posts based on username (unprotected endpoint)
