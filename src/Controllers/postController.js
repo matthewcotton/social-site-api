0872ac1bd74fbdc1ed25c3171f7dec048405643a
matthewcotton <matthewneilcotton@gmail.com>
2021-03-08 13:54:23 +0000

    switch to sending params

--- src/Controllers/postController.js
+++ src/Controllers/postController.js
@@ -5,8 +5,8 @@ const authController = require("./authController");
 
 // Get all posts (unprotected endpoint)
 exports.index = async function (req, res) {
-  const skip = req.body.skip ? Number(req.body.skip) : 0;
-  const limit = req.body.limit ? Number(req.body.limit) : 10;
+  const skip = req.params.skip ? Number(req.body.skip) : 0;
+  const limit = req.params.limit ? Number(req.body.limit) : 10;
   console.log(req.body)
   console.log(`skip: ${skip}  limit: ${limit}`)
   res.send(await Post.find({}).sort({ timestamp: -1 }).skip(skip).limit(limit));
