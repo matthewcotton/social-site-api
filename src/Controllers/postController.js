be898c7c00273e8473a4a267e56fb283a832ce4e
matthewcotton <matthewneilcotton@gmail.com>
2021-03-08 13:40:58 +0000

    log skip and limit agrs

--- src/Controllers/postController.js
+++ src/Controllers/postController.js
@@ -5,9 +5,9 @@ const authController = require("./authController");
 
 // Get all posts (unprotected endpoint)
 exports.index = async function (req, res) {
-  console.log(`skip: ${skip}  limit: ${limit}`)
   const skip = req.body.skip ? Number(req.body.skip) : 0;
   const limit = req.body.limit ? Number(req.body.limit) : 10;
+  console.log(`skip: ${skip}  limit: ${limit}`)
   res.send(await Post.find({}).sort({ timestamp: -1 }).skip(skip).limit(limit));
 };
 
